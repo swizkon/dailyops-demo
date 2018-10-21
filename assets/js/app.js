9e2759d87dab07299b56cb7d9de380978468ad7f
swizkon <jonas@jerndin.se>
2018-10-22 01:12:59 +0200

    New version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,4 @@
 
-var protodata = {
-    title: "Welcome back",
-    username: "Malloc Frobnitz",
-    journeys: [
-        {
-            "page": "#azure",
-            "name": "Azure exam: Setup a three node Eventstore cluster",
-            "progress": "10",
-            "stepName": "Build a docker image",
-            "stepState": "In progress",
-            "stops": [
-                {
-                    "title": ""
-                }
-            ]
-        },
-        {
-            "page": "#guitar",
-            "name": "Guitar 101: 4 chords for 1000 songs",
-            "progress": "60",
-            "stepName": "Learn the G-chord",
-            "stepState": "Next step",
-            "stops": [
-                {
-                    "title": ""
-                }
-            ]
-        }
-    ]
-};
-
 var formatDays = function(days) {
 return (days == 0) ? "Today!"
         : (days == 1) ? "Tomorrow"
@@ -40,16 +9,6 @@ var today = new Date(Date.now());
 today.setHours(0);
 today.setMinutes(0);
 today.setSeconds(1);
-
-var anniversaries =
-    {
-        "2015-02-22T22:11:00.000Z": "Signes birthday",
-        "2009-04-04T03:42:00.000Z": "Desmonds birthday",
-        "2005-07-03T18:15:00.000Z": "J+J first date",
-        "2011-11-05T18:00:00.000Z": "J+J wedding day",
-        "1980-06-01T08:00:00.000Z": "Jenny´s birthday",
-        "1980-05-16T12:00:00.000Z": "Jonas birthday"
-    };
 
 var sorted = [];
 
@@ -83,19 +42,9 @@ var days = b.diff(a, 'days');
 document.querySelector("#page-title").textContent = protodata.title;
 document.querySelector("#page-subtitle").textContent = protodata.username;
 
-document.querySelector("#journey_count").textContent = protodata.journeys.length;
 
 var upcoming = "", journey;
 
-for(var journeryIndex in protodata.journeys) {
-    journey  =protodata.journeys[journeryIndex];
-    upcoming += "<hr />" 
-    upcoming += "<a href=\"" + journey.page + "\">";
-    upcoming += "<h4>" + journey.name + " <small>" + journey.progress  + "%</small></h4>";
-    upcoming += "<h1><small>" + journey.stepState  + ":</small> " + journey.stepName + " </h1>";
-    upcoming +=  progress(journey.progress);
-    upcoming += "</a>";
-}
 
 function progress(percent) {
 
@@ -103,6 +52,3 @@ function progress(percent) {
     + "<div class=\"progress-bar\" role=\"progressbar\" style=\"width: " + percent + "%\" aria-valuenow=\"25\" aria-valuemin=\"0\" aria-valuemax=\"100\"></div>"
     + "</div>";
 }
-
-
-document.querySelector("#journeys").innerHTML = upcoming;
